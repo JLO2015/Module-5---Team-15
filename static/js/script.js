67b54b6123bee2db37656c9f4cf43f0a87039095
JLO2015 <jeromelasernaolvario@gmail.com>
2015-08-31 22:12:35 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,20 +3,18 @@ $(function(){
 
 		var data = $(event.target).serializeArray();
 
-		var student = {};
+		var thesis= {};
 
 		for(var i = 0; i<data.length ; i++){
-			student[data[i].name] = data[i].value;
+			thesis[data[i].name] = data[i].value;
 		}
 
-		// send data to server
 			var thesis_create_api = '/api/thesis';
-			$.post(thesis_create_api, student, function(response){
+			$.post(thesis_create_api, thesis, function(response){
 
-			// read response from server
 			if (response.status = 'OK') {
 				var thesisDetail = '<strong>YEAR:</strong> ' + response.data.Year + '<br><strong>TITLE: </strong>' + response.data.Title + '<br><strong>CREATED BY: </strong>' + response.data.first_name + ' ' + response.data.last_name;
-				$('#student-list').prepend('<li>' + thesisDetail + '<br><a href=\"/thesis/delete/'+response.data.id+'\"><button id=\"delete\" class=\"btn\" type=\"submit\">DELETE</button></a> <a href=\"/thesis/edit/'+response.data.id+'\"><button id=\"edit\" class=\"btn\" type=\"submit\">EDIT</button></a><hr></li>')
+				$('#thesis-list').prepend('<li>' + thesisDetail + '<br><a href=\"/thesis/delete/'+response.data.id+'\"><button id=\"delete\" class=\"btn\" type=\"submit\">DELETE</button></a> <a href=\"/thesis/edit/'+response.data.id+'\"><button id=\"edit\" class=\"btn\" type=\"submit\">EDIT</button></a><hr></li>')
 				$('input[type=text], [type=number]').val('');
 				$('textarea[type=text]').val('');
 				$('select[name=Year]').val('Year');
@@ -33,10 +31,10 @@ $(function(){
 	function loadThesis(){
 		var thesis_list_api = '/api/thesis';
 		$.get(thesis_list_api, {} , function(response) {
-			console.log('#student-list', response)
+			console.log('#thesis-list', response)
 			response.data.forEach(function(thesis){
 				var thesisDetail = '<strong>YEAR: </strong>' + thesis.Year + '<br><strong>TITLE: </strong>'  + thesis.Title + '<br><strong>CREATED BY: </strong>' + thesis.first_name + ' ' + thesis.last_name;
-				$('#student-list').append('<li>' + thesisDetail + '<br><a href=\"/thesis/delete/'+thesis.id+'\"><button id=\"delete\" class=\"btn\" type=\"submit\">DELETE</button></a> <a href=\"/thesis/edit/'+thesis.id+'\"><button id=\"edit\" class=\"btn\" type=\"submit\">EDIT</button></a><hr></li>')
+				$('#thesis-list').append('<li>' + thesisDetail + '<br><a href=\"/thesis/delete/'+thesis.id+'\"><button id=\"delete\" class=\"btn\" type=\"submit\">DELETE</button></a> <a href=\"/thesis/edit/'+thesis.id+'\"><button id=\"edit\" class=\"btn\" type=\"submit\">EDIT</button></a><hr></li>')
 			});
 		});
 	}
